5a0e421a79736c4226e3d26ae1e3ea57018e8ca3
zhang_jin[zhang_jin@suixingpay.com] <zhang_jin[zhang_jin@suixingpay.com]@LAPTOP-RR6IOJFV>
2017-09-19 19:22:34 +0800

    add annotation ,io

---
+++ src/annotation/Tag.java
@@ -0,0 +1,16 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE,ElementType.METHOD})
+public @interface Tag {
+	String name() default "zhangsan";
+	String desc() default "good";
+
+}
