bca619d36b36cda532df9cae4527efa8a14c9cd9
zhang_jin[zhang_jin@suixingpay.com] <zhang_jin[zhang_jin@suixingpay.com]@LAPTOP-RR6IOJFV>
2017-09-21 17:07:06 +0800

    关于 simpleDateFormat类的线程不安全问题，解决方案

---
+++ src/dateformat/DateUtil3.java
@@ -0,0 +1,29 @@
+package dateformat;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/**
+ * 采用static静态变量:不是线程安全的。
+ * ==>解决方案：同步锁
+ * 　说明：当线程较多时，当一个线程调用该方法时，其他想要调用此方法的线程就要block，
+ * 多线程并发量大的时候会对性能有一定的影响。
+ * @author zhang_jin[zhang_jin@suixingpay.com]
+ *
+ */
+public class DateUtil3 {
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	public static String formateDate(Date date){
+		synchronized (sdf) {
+			return sdf.format(date);
+		}
+	}
+	
+	public static Date parse(String dateStr) throws ParseException{
+		synchronized (sdf) {
+			return sdf.parse(dateStr);
+		}
+	}
+
+}
---
+++ src/dateformat/DateUtilTest.java
@@ -0,0 +1,30 @@
+package dateformat;
+
+public class DateUtilTest {
+	
+	public static class TestThreadSafe extends Thread{
+		@Override
+		public void run() {
+			while(true){
+				try {
+					this.join(2000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				try {
+					System.out.println(this.getName()+":"+DateUtil3.parse("2013-05-24 06:02:20"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			new TestThreadSafe().start();
+		}
+	}
+
+}
