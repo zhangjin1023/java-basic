556298f752edf79ebf38e2142336c972fa8714e9
zhang_jin[zhang_jin@suixingpay.com] <zhang_jin[zhang_jin@suixingpay.com]@LAPTOP-RR6IOJFV>
2017-09-21 11:55:22 +0800

    自定义注解 案例

---
+++ src/annotation/ext/FruitName.java
@@ -0,0 +1,23 @@
+package annotation.ext;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 水果名称注解
+ * 
+ * @author zhang_jin[zhang_jin@suixingpay.com]
+ *
+ */
+@Target(ElementType.FIELD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FruitName {
+	String value() default "";
+
+}
