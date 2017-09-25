26d64504b8b41ce0a92bc58ef146a54576ef05f5
zhangjin1023 <514257236@qq.com>
2017-09-26 00:44:02 +0800

    兔子问题，压缩合成图片问题

--- src/oop/rabbit/Farm.java
+++ src/oop/rabbit/Farm.java
@@ -1,10 +1,3 @@
-/**  
- * All rights Reserved, Designed By Suixingpay.
- * @author: zhang_jin[zhang_jin@suixingpay.com] 
- * @date: 2017年9月25日 下午8:42:09   
- * @Copyright ©2017 Suixingpay. All rights reserved. 
- * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
- */
 package oop.rabbit;
 
 import java.util.ArrayList;
