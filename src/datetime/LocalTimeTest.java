f5f081029df0fb0e63986db1739d98f96d0c9b3d
zhang_jin[zhang_jin@suixingpay.com] <zhang_jin[zhang_jin@suixingpay.com]@windows10.microdone.cn>
2017-10-17 14:26:20 +0800

    java 8 日期、时间api实例

---
+++ src/datetime/LocalTimeTest.java
@@ -0,0 +1,12 @@
+package datetime;
+
+import java.time.LocalTime;
+
+public class LocalTimeTest {
+	public static void main(String[] args) {
+		//1.current time
+		LocalTime now = LocalTime.now();
+		System.out.println(now);
+	}
+
+}
